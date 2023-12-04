94bb715913d48d1d8c144eeb44bf4335ea13e5e1
Totty <kaqiu96@gmail.com>
2023-12-04 17:26:38 +0100

    created project alternative-security

---
+++ src/main/java/com/example/alternativesecurity/AlternativeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.alternativesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternativeSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlternativeSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alternativesecurity/AlternativeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alternativesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlternativeSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
